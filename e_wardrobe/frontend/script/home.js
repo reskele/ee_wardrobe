b30cfbc6ee067bcecd10af985d12b3a694681591
reskele <revocatuspatrick14@gmail.com>
2025-04-01 15:17:10 -0700

    arrange section

---
+++ e_wardrobe/frontend/script/home.js
@@ -0,0 +1,17 @@
+
+  function openNav() {
+
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("overlay").style.display = "block";
+  }
+
+  function closeNav()  {
+
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
+
+
+  }
+
+
+ 
